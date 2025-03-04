a0f079009ab87bda1033306e3f02f85b6058b204
Kiet <haokietho2000@gmail.com>
2025-03-04 21:07:25 +0700

    asdasd

---
+++ Assets/Backup/Player/Animation/AnimationName.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+[CreateAssetMenu(fileName = "AnimationName", menuName = "Data/Player Data/Animation/Name")]
+public class AnimationName : ScriptableObject
+{
+
+}
