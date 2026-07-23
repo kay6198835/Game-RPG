b33fe724eceba94c6275820621205f00d32d9c18
VIETUNION\kiet.ho <kiet.ho@vietunion.com.vn>
2026-07-23 16:38:06 +0700

    add player lifecycle

---
+++ Assets/Script/Character/Player/States/PlayerDeathState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathState : PlayerDisadvantageState
+{
+    public PlayerDeathState(Player player, string animBoolName) : base(player, animBoolName)
+    {
+    }
+    public override void Enter()
+    {
+        base.Enter();
+    }
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (StatusAnimation.Start)
+        {
+            EventManager.Emit(EventID.ON_PLAYER_DEATH);
+        }
+        if (StatusAnimation.End)
+        {
+            EventManager.Emit(EventID.ON_REALOAD_GAME);
+        }
+    }
+}
