f40b49179ddcec2d1f70a7837e833509cbb17bfe
VIETUNION\kiet.ho <kiet.ho@vietunion.com.vn>
2026-07-31 17:32:11 +0700

    coding

--- Assets/Script/Character/Player/CoreComponent/WeaponHolder.cs
+++ Assets/Script/Character/Player/CoreComponent/WeaponHolder.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 
 public class WeaponHolder : Interact
 {
-    [SerializeField] public  Weapon Weapon {get; private set;}
+    [SerializeField] public Weapon Weapon { get; private set; }
 
     protected override void Awake()
     {
         base.Awake();
-        interactableMask = LayerMask.GetMask("Weapon"); 
+        interactableMask = LayerMask.GetMask("Weapon");
     }
     public void Equid_UnEquid(Weapon weapon)
     {
-        if(this.Weapon == null)
+        if (this.Weapon == null)
         {
             this.Weapon = weapon;
-        }else
+        }
+        else
         {
             this.Weapon = null;
         }
     }
     public override void Intertion()
     {
-        if(Weapon != null)
+        if (Weapon != null)
         {
             Weapon.UnEquid();
 
         }
-        if(Weapon == null)
+        if (Weapon == null)
         {
             base.Intertion();
         }
     }
+
+    public void Attack()
+    {
+        if (Weapon == null) return;
+        Weapon.SetAnimation(player);
+    }
+
+    
 }
