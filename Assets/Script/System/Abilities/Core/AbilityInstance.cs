de9817321c4addc70d0c933c379bf5397f4830fb
VIETUNION\kiet.ho <kiet.ho@vietunion.com.vn>
2026-09-17 15:31:35 +0700

    Revert "fix(abilities): inline hold-ratio math after AbilityRuntimeHelpers removal"
    This reverts commit 1de8ed6550a13b0abd90295385220a682357dd79.

--- Assets/Script/System/Abilities/Core/AbilityInstance.cs
+++ Assets/Script/System/Abilities/Core/AbilityInstance.cs
@@ -40,9 +40,7 @@ public class AbilityInstance
         RefreshHoldContext();
     }
 
-    public float HoldRatio => Definition.MaxHoldTime <= 0f
-        ? 0f
-        : Mathf.Clamp01(CurrentHoldTime / Definition.MaxHoldTime);
+    public float HoldRatio => AbilityRuntimeHelpers.SafeRatio(CurrentHoldTime, Definition.MaxHoldTime);
 
     // Context is built once per cast, so charge-scaling effects only see a live hold value
     // if it is pushed back into the context every tick.
